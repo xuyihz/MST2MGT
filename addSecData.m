1dedc4d677f99a64d0f7a684b4e84bad3faf4b02
Xu Yi <xuyi@ziad.cn>
2020-01-15 20:31:16 +0800

    read me again

--- addSecData.m
+++ addSecData.m
@@ -1,10 +1,10 @@
 %% function
 %
-% write the BOX section data into MGTstr
+% add the BOX section data into MGTstr
 % Xu Yi, 2020
 
 %%
-function newMGTstr = writeSecData(MGTstr, SecLine_end, NumMatrix, SecMatrix)
+function newMGTstr = addSecData(MGTstr, SecLine_end, NumMatrix, SecMatrix)
 for i = 1:length(NumMatrix)
     SecName = sprintf('B %dx%dx%dx%d',SecMatrix(i,1),SecMatrix(i,2),SecMatrix(i,3),SecMatrix(i,4));
     secLOGstr(i,1) = sprintf("   %d,DBUSER,%s,CC,0,0,0,0,0,0,YES,NO,B,2,%f,%f,%f,%f,0,0,0,0,0,0",...
